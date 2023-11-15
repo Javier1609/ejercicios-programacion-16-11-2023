adb0b67f6efe879214f55116a4b9af43ba2bda44
Javier1609 <jvrgmz@myuax.com>
2023-11-15 18:18:40 +0100

    ejercicio 1

--- ejercicio_1.cpp
+++ ejercicio_1.cpp
@@ -10,10 +10,30 @@ namespace Matematicas {
     int resta(int a, int b) {
         return a - b;
     }
+
+    int multiplicacion (int a, int b) {
+        return a * b;
+    }
+
+    int division (int a, int b) {
+        return a / b;
+    }
+
+    int modulo (int a, int b) {
+        return a % b;
+    }
+    int potencia (int a, int b) {
+        return a ^ b;
+    }
 }
 
 int main() {
     std::cout << "Suma: " << Matematicas::suma(8, 4) << std::endl;
     std::cout << "Resta: " << Matematicas::resta(8, 4) << std::endl;
+    std::cout << "Multiplicacion: " << Matematicas::multiplicacion(8, 4) << std::endl;
+    std::cout << "Division: " << Matematicas::division(8, 4) << std::endl;
+    std::cout << "Modulo: " << Matematicas::modulo(8, 4) << std::endl;
+    std::cout << "Potencia: " << Matematicas::potencia(8, 4) << std::endl;
+
     return 0;
 }
